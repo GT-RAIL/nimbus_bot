2706fd5f60ab08293352f9ace89ba7263398909b
dekent <dekent90@gmail.com>
2016-08-04 10:26:38 -0400

    WORKING INTERFACE DEMO CODE; see GT-RAIL/puzzlebot_interface and gt-rail-internal/codebase for other components

--- nimbus_interactive_manipulation/src/nimbus6dofVis.cpp
+++ nimbus_interactive_manipulation/src/nimbus6dofVis.cpp
@@ -20,6 +20,7 @@ Nimbus6dofVis::Nimbus6dofVis() : pnh("~")
 void Nimbus6dofVis::markerPositionCallback(const geometry_msgs::PoseStamped& pose)
 {
   imServer->setPose("nimbus_gripper", pose.pose);
+  imServer->applyChanges();
 }
 
 void Nimbus6dofVis::makeGripperMarker()
@@ -39,24 +40,61 @@ void Nimbus6dofVis::makeGripperMarker()
   iMarker.name = "nimbus_gripper";
   iMarker.description = "View Nimbus' planned gripper pose";
 
+//  visualization_msgs::Marker gripperMarker;
+//  gripperMarker.pose.position.x = -0.125;
+//  gripperMarker.pose.orientation.x = 0.7071;
+//  gripperMarker.pose.orientation.w = 0.7071;
+//  gripperMarker.type = visualization_msgs::Marker::MESH_RESOURCE;
+//  gripperMarker.mesh_resource = "package://agile_test_nodes/meshes/robotiq_85_base_link.dae";
+//  gripperMarker.scale.x = 1.0;
+//  gripperMarker.scale.y = 1.0;
+//  gripperMarker.scale.z = 1.0;
+//  gripperMarker.color.r = 1.0;
+//  gripperMarker.color.g = 1.0;
+//  gripperMarker.color.b = 0.0;
+//  gripperMarker.color.a = 0.5;
   visualization_msgs::Marker gripperMarker;
-  gripperMarker.pose.position.x = -0.125;
-  gripperMarker.pose.orientation.x = 0.7071;
-  gripperMarker.pose.orientation.w = 0.7071;
-  gripperMarker.type = visualization_msgs::Marker::MESH_RESOURCE;
-  gripperMarker.mesh_resource = "package://agile_test_nodes/meshes/robotiq_85_base_link.dae";
-  gripperMarker.scale.x = 1.0;
-  gripperMarker.scale.y = 1.0;
-  gripperMarker.scale.z = 1.0;
-  gripperMarker.color.r = 1.0;
-  gripperMarker.color.g = 1.0;
-  gripperMarker.color.b = 0.0;
+  gripperMarker.pose.orientation.w = 1.0;
+  gripperMarker.type = visualization_msgs::Marker::CUBE;
+  gripperMarker.scale.x = 0.076;
+  gripperMarker.scale.y = 0.02;
+  gripperMarker.scale.z = 0.11;
+  gripperMarker.color.r = 0.75;
+  gripperMarker.color.g = 0.0;
+  gripperMarker.color.b = 0.75;
   gripperMarker.color.a = 0.5;
+  visualization_msgs::Marker gripperFingerL;
+  gripperFingerL.pose.position.x = 0.065;
+  gripperFingerL.pose.position.z = -0.05;
+  gripperFingerL.pose.orientation.w = 1.0;
+  gripperFingerL.type = visualization_msgs::Marker::CUBE;
+  gripperFingerL.scale.x = 0.052;
+  gripperFingerL.scale.y = 0.02;
+  gripperFingerL.scale.z = 0.01;
+  gripperFingerL.color.r = 0.75;
+  gripperFingerL.color.g = 0.0;
+  gripperFingerL.color.b = 0.75;
+  gripperFingerL.color.a = 0.5;
+  visualization_msgs::Marker gripperFingerR;
+  gripperFingerR.pose.position.x = 0.065;
+  gripperFingerR.pose.position.z = 0.05;
+  gripperFingerR.pose.orientation.w = 1.0;
+  gripperFingerR.type = visualization_msgs::Marker::CUBE;
+  gripperFingerR.scale.x = 0.052;
+  gripperFingerR.scale.y = 0.02;
+  gripperFingerR.scale.z = 0.01;
+  gripperFingerR.color.r = 0.75;
+  gripperFingerR.color.g = 0.0;
+  gripperFingerR.color.b = 0.75;
+  gripperFingerR.color.a = 0.5;
 
   visualization_msgs::InteractiveMarkerControl gripperControl;
   gripperControl.markers.push_back(gripperMarker);
+  gripperControl.markers.push_back(gripperFingerL);
+  gripperControl.markers.push_back(gripperFingerR);
   gripperControl.interaction_mode = visualization_msgs::InteractiveMarkerControl::NONE;
   gripperControl.name = "gripper_control";
+  gripperControl.always_visible = true;
 
   iMarker.controls.push_back(gripperControl);
 
