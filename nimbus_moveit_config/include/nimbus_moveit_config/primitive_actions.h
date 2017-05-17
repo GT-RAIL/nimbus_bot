7503287ce64b1cecd7105a8b53db258da84164ec
David Kent <davidkent@wpi.edu>
2017-05-17 11:56:28 -0400

    Initial update of MoveIt action wrappers for 7DOF arm

---
+++ nimbus_moveit_config/include/nimbus_moveit_config/primitive_actions.h
@@ -0,0 +1,36 @@
+#ifndef PRIMITIVE_ACTIONS_H_
+#define PRIMITIVE_ACTIONS_H_
+
+#include <ros/ros.h>
+#include <actionlib/server/simple_action_server.h>
+#include <rail_manipulation_msgs/PrimitiveAction.h>
+#include <rail_manipulation_msgs/CartesianPath.h>
+#include <std_srvs/Empty.h>
+#include <tf/transform_listener.h>
+
+class PrimitiveActions
+{
+public:
+  /**
+  * \brief Constructor
+  */
+  PrimitiveActions();
+
+private:
+  ros::NodeHandle n, pnh;
+  //ros::Publisher angularCmdPublisher;
+
+  ros::ServiceClient eraseTrajectoriesClient;
+  ros::ServiceClient cartesianPathClient;
+  //ros::ServiceClient jacoPosClient;
+
+  actionlib::SimpleActionServer<rail_manipulation_msgs::PrimitiveAction> primitiveServer;
+
+  tf::TransformListener tfListener;
+
+  static const int NUM_JACO_JOINTS = 7;
+
+  void executePrimitive(const rail_manipulation_msgs::PrimitiveGoalConstPtr &goal);
+};
+
+#endif
